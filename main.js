45e5fed57373af4e42dc26966821ff58d56bd71f
DanielwasTaken <53798206+DanielwasTaken@users.noreply.github.com>
2021-02-15 21:07:53 -0800

    Update main.js

--- main.js
+++ main.js
@@ -27,7 +27,7 @@ function changeNumPomo(x) {
         threePomoOption.style.backgroundColor = "white";
         fourPomoOption.style.backgroundColor = "white";
     } else if (x.id == "three-pomo-option") {
-        localStorage.setItem("pomoCount", "three-pomo-option");
+        localStorage.setItem("pomoCount", 3);
         thirdPomo.style.visibility = "visible";
         fourthPomo.style.visibility = "hidden";
         
